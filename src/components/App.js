bbc73118f1750e24b2052f09a81878de621530eb
Fernando Caldera <fcaldera@cebroker.com>
2016-08-16 12:00:37 -0500

    config approach refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import logo from '../images/logo.svg';
 import './App.css';
-import EnvironmentLabel from './EnvironmentLabel';
 
 class App extends Component {
   render() {
+    let env = null;
+    if (process.env.REACT_APP_ENV !== 'production') {
+      env = <span className="App-env-label">{process.env.REACT_APP_ENV || "development"}</span>
+    }
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
-          <EnvironmentLabel />
+          {env}
         </div>
         <p className="App-intro">
           To get started, edit <code>src/components/App.js</code> and save to reload. Then navigate using the links below.
